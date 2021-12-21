67af4b73359368c7bc0b4aed5a81c9cae7460052
User <User@LORENA-CIOATA-L>
2021-12-21 17:17:59 +0200

    Add project files.

---
+++ OpenTK_winforms_z03/Piramida.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_winforms_z03
+{
+    public class Piramida
+    {
+        public Punct A = new Punct(0, 0, 0);
+        public Punct B = new Punct(0, 50, 0);
+        public Punct C = new Punct(50, 0, 0);
+        public Punct D = new Punct(50, 50, 0);
+        public Punct E = new Punct(25, 25, 50);
+    }
+}
---
+++ OpenTK_winforms_z03/Punct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_winforms_z03
+{
+    public class Punct
+    {
+        public int x;
+        public int y;
+        public int z;
+        public Punct(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
